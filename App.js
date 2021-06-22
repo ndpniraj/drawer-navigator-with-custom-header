4af7d6729cf6dc1e8e71c5589ca9f624d32e7588
ndpniraj <ndpniraj@gmail.com>
2021-06-22 18:49:16 +0530

    added drawer navigation with custom header and footer

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Image, TouchableOpacity, Button } from 'react-native';
 
-export default function App() {
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home</Text>
+      {/* <Button onPress={() => navigation.toggleDrawer()} title='test' /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Tasks() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tasks</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+const CustomDrawer = props => {
+  return (
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView {...props}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: 20,
+            backgroundColor: '#f6f6f6',
+            marginBottom: 20,
+          }}
+        >
+          <View>
+            <Text>John Doe</Text>
+            <Text>example@email.com</Text>
+          </View>
+          <Image
+            source={{
+              uri: 'https://images.unsplash.com/photo-1624243225303-261cc3cd2fbc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+            }}
+            style={{ width: 60, height: 60, borderRadius: 30 }}
+          />
+        </View>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          right: 0,
+          left: 0,
+          bottom: 50,
+          backgroundColor: '#f6f6f6',
+          padding: 20,
+        }}
+      >
+        <Text>Log Out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: 'transparent',
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        headerTitle: '',
+      }}
+      drawerContent={props => <CustomDrawer {...props} />}
+    >
+      <Drawer.Screen component={Home} name='Home' />
+      <Drawer.Screen component={Tasks} name='Tasks' />
+    </Drawer.Navigator>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigator />
+    </NavigationContainer>
+  );
+}
